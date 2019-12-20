daa2cf01174b5f659462a6f60795b5c2431f0283
hfurumiya <bosnyctokosa@gmail.com>
2019-12-20 14:37:36 +0000

    12/20分まで

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,27 @@
 class TasksController < ApplicationController
+  before_action :set_user
+
+  def index
+    @tasks = @user.tasks
+  end
+
   
   def new
     @task = Task.new
   end
   
   def create
-    @task = @user.tasks.build(params[:id])
+    @task = @user.tasks.build(params[:task])
     if @task.save
       flash[:success] = "タスクを新規作成しました。"
-      redirect_to '#'
+      redirect_to user_task_url(@user, @task)
     else
       render 'new'
     end
   end
   
-  def index
-    @tasks = Task.all
-  end
-  
+  private
+    def set_user
+      @user = User.find(params[:user_id])
+    end
 end
