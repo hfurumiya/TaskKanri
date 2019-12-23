0ed316c66bf7d5dd3544364f6b04ddc33336b7b8
hfurumiya <bosnyctokosa@gmail.com>
2019-12-23 08:08:46 +0000

    見栄え悪いがC10までと思う

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_user
 
+
   def index
     @tasks = @user.tasks
   end
@@ -11,17 +12,48 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = @user.tasks.build(params[:task])
+    @task = @user.tasks.build(task_params)
     if @task.save
       flash[:success] = "タスクを新規作成しました。"
-      redirect_to user_task_url(@user, @task)
+      redirect_to user_tasks_url(@user, @task)
     else
       render 'new'
     end
   end
   
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:success] = "タスクを更新しました"
+      redirect_to user_tasks_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    if @task.save
+      flash[:success] = "消しましたよ！"
+      redirect_to user_task_path
+    end
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
   private
     def set_user
       @user = User.find(params[:user_id])
+    end
+    
+    def task_params
+      params.require(:task).permit(:title, :body)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,6 @@ User.create!( name: "Sample User",
               email: "sample@email.com",
               password: "password",
               password_confirmation: "password")
+              
+User.first.tasks.create!( title: "test title1",
+              body: "test body1")
