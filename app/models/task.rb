e86576cc4b73a6feb8edf8401eeb06a4d66ea597
hfurumiya <bosnyctokosa@gmail.com>
2020-01-09 13:18:24 +0000

    G24までした

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
   
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true, length: { minimum: 5 }
